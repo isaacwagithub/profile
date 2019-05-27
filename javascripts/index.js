72f5c4d420963bbbfa17547751e93cbb6dc2293c
isaacwagithub <imokoena268@gmail.com>
2019-05-27 13:07:13 +0200

    Add files via upload

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-	console.log('hello');
+	
+	$("li.page").click(function(event){
+		event.preventDefault();
+		
+		let aboutMe;
+		
+		var liClicked = $(this);
+		var liClickedAttr = liClicked[0].attributes;
+		
+		var liOwnerElement = liClickedAttr[0].ownerElement;
+		var liClassName = liOwnerElement.classList[0];
+		
+		var aboutMeesDivs = document.getElementsByClassName('aboutMees');
+		
+		for(var i=0;i<aboutMeesDivs.length;i++){
+			aboutMe = aboutMeesDivs[i];
+			if($(aboutMe).hasClass(liClassName)){
+				$(aboutMe).removeClass('hideIt');
+			}else{
+				$(aboutMe).addClass('hideIt');
+			}
+		}
+		
+	});
+	//end of page click function
 });
